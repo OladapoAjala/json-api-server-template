b003afa177ea0041c96860dd25be82f07df014eb
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-07-24 23:50:12 +0100

    added simple comments

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/**
+ * This file contains code for express server.
+ * Includes:
+ *    1. Route handlers
+ *    2. Data sanitization
+ *    3. Prevention against parameter pollution
+ *    4. Rate limit
+ */
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,16 +21,19 @@ const globalErrorHandler = require('./controllers/errorController');
 // Import route handlers (here's a sample route for user)
 const userRouter = require('./routes/userRoutes');
 
+// Create an instance of express
 const app = express();
 
 // MIDDLEWARES
 // console.log(process.env.NODE_ENV);
 app.use(helmet());
 
+// Log client requests if server is running in development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit total requests from a single IP address (after 100 requests wait for an hour before logging another request)
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -54,7 +65,6 @@ app.use(
   })
 );
 
-/***************************************************/
 // Use this to serve static files to the client
 app.use(express.static(`${__dirname}/public`));
 
