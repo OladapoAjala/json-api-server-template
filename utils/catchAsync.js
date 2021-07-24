d9086da5f86ab79a246c97815cb2b2881f1abfae
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-07-24 23:53:44 +0100

    added documentation using JSDoc

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,8 @@
-/**********
+/**
  * Async error handling function
- **********/
+ * @module utils/catchAsync
+ * @returns {function}
+ */
 module.exports = (fn) => (req, res, next) => {
   fn(req, res, next).catch(next);
 };
